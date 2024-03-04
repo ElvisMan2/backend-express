b725d51a8dc71738d2209f9364c9896f3eb122ab
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2024-03-04 15:14:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const usuarioRoutes= require('./routes/usuario')
 const express= require("express");
 const app=express();
-const PORT=3000;
+const PORT=process.env.PORT||3000;
 usuarioRoutes(app);
 
 app.listen(PORT);
